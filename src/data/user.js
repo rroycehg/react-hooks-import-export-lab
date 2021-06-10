7ba67e2a31556e4241d9572f18669cfdcb3a37d7
Roli <rroycehg@gmail.com>
2021-06-09 22:22:03 -0400

    Completed Assignment

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,7 @@
-const username = "Liza";
+const username = "Roli";
 const city = "New York";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+const image = "https://media.discordapp.net/attachments/779249322071031819/852175042627895316/unknown.png";
 
 // export all three variables as named exports
+export { username, city, image };
